f55ad9c9b261a7e164a41b121cb695750d705e28
Jaran Nilsen <jaran.nilsen@gmail.com>
2012-11-05 20:01:03 +0100

    implemented method for enumerating WPD devices

--- wpd-lib/StandardWindowsPortableDeviceService.cs
+++ wpd-lib/StandardWindowsPortableDeviceService.cs
@@ -1,13 +1,39 @@
 ﻿using System;
+using WindowsPortableDevicesLib.Domain;
+using PortableDeviceApiLib;
 
 namespace WindowsPortableDevicesLib
 {
 
-    public class StandardWindowsPortableDeviceService
+    public class StandardWindowsPortableDeviceService : WindowsPortableDeviceService
     {
+        private PortableDeviceManager portableDeviceManager = new PortableDeviceManagerClass();
+        
         public StandardWindowsPortableDeviceService()
         {
             
+        }
+        
+        public string[] DeviceIDs {
+            get {
+                portableDeviceManager.RefreshDeviceList();
+
+                // Determine how many WPD devices are connected
+                var deviceIds = new string[1];
+                uint count = 1;
+                portableDeviceManager.GetDevices(ref deviceIds[0], ref count);
+
+                // Retrieve the device id for each connected device
+                deviceIds = new string[count];
+                portableDeviceManager.GetDevices(ref deviceIds[0], ref count);
+                
+                return deviceIds;
+            }
+        }
+        
+        public WindowsPortableDevice ConnectDevice(string deviceID)
+        {
+            throw new NotImplementedException();
         }
     }
 }
