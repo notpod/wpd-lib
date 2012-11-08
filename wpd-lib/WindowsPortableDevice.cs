2e02d5e3b7ee02735d76fe7f1fb94b6003dbbb90
Jaran Nilsen <jaran.nilsen@gmail.com>
2012-11-08 19:20:06 +0100

    added support for transfering content to device.

--- wpd-lib/WindowsPortableDevice.cs
+++ wpd-lib/WindowsPortableDevice.cs
@@ -9,6 +9,7 @@
 using System;
 using PortableDeviceApiLib;
 using Common.Logging;
+using System.IO;
 
 namespace WindowsPortableDevicesLib.Domain
 {
@@ -104,6 +105,129 @@ namespace WindowsPortableDevicesLib.Domain
             EnumerateContents(ref content, root);
 
             return root;
+        }
+        
+        public void GetFile(PortableDeviceFile file, string saveToPath)
+        {
+            IPortableDeviceContent content;
+            device.Content(out content);
+
+            IPortableDeviceResources resources;
+            content.Transfer(out resources);
+            
+            PortableDeviceApiLib.IStream wpdStream;
+            uint optimalTransferSize = 0;
+
+            var property = new _tagpropertykey();
+            property.fmtid = new Guid(0xE81E79BE, 0x34F0, 0x41BF, 0xB5, 0x3F,
+                                      0xF1, 0xA0, 0x6A, 0xE8, 0x78, 0x42);
+            property.pid = 0;
+
+            resources.GetStream(file.Id, ref property, 0, ref optimalTransferSize,
+                                out wpdStream);
+
+            System.Runtime.InteropServices.ComTypes.IStream sourceStream =
+                (System.Runtime.InteropServices.ComTypes.IStream) wpdStream;
+
+            var filename = Path.GetFileName(file.Id);
+            FileStream targetStream = new FileStream(Path.Combine(saveToPath, filename),
+                                                     FileMode.Create, FileAccess.Write);
+            
+            unsafe
+            {
+                var buffer = new byte[1024];
+                int bytesRead;
+                do
+                {
+                    sourceStream.Read(buffer, 1024, new IntPtr(&bytesRead));
+                    targetStream.Write(buffer, 0, 1024);
+                } while (bytesRead > 0);
+
+                targetStream.Close();
+            }
+        }
+        
+        public void TransferContentToDevice(string fileName,
+                                            string parentObjectId) {
+            
+            IPortableDeviceContent content;
+            device.Content(out content);
+            
+            IPortableDeviceValues values =
+                GetRequiredPropertiesForContentType(fileName, parentObjectId);
+            
+            PortableDeviceApiLib.IStream tempStream;
+            uint optimalTransferSizeBytes = 0;
+            content.CreateObjectWithPropertiesAndData(
+                values,
+                out tempStream,
+                ref optimalTransferSizeBytes,
+                null);
+            
+            System.Runtime.InteropServices.ComTypes.IStream targetStream =
+                (System.Runtime.InteropServices.ComTypes.IStream) tempStream;
+            
+            try
+            {
+                using (var sourceStream =
+                       new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    var buffer = new byte[optimalTransferSizeBytes];
+                    int bytesRead;
+                    do
+                    {
+                        bytesRead = sourceStream.Read(
+                            buffer, 0, (int)optimalTransferSizeBytes);
+                        IntPtr pcbWritten = IntPtr.Zero;
+                        targetStream.Write(
+                            buffer, (int)optimalTransferSizeBytes, pcbWritten);
+                    } while (bytesRead > 0);
+                }
+                targetStream.Commit(0);
+            }
+            finally
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(tempStream);
+            }
+        }
+        
+        private IPortableDeviceValues GetRequiredPropertiesForContentType(
+            string fileName,
+            string parentObjectId)
+        {
+            IPortableDeviceValues values =
+                new PortableDeviceTypesLib.PortableDeviceValues() as IPortableDeviceValues;
+            
+            var WPD_OBJECT_PARENT_ID = new _tagpropertykey();
+            WPD_OBJECT_PARENT_ID.fmtid =
+                new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC,
+                         0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            WPD_OBJECT_PARENT_ID.pid = 3 ;
+            values.SetStringValue(ref WPD_OBJECT_PARENT_ID, parentObjectId);
+
+            FileInfo fileInfo = new FileInfo(fileName);
+            var WPD_OBJECT_SIZE = new _tagpropertykey();
+            WPD_OBJECT_SIZE.fmtid =
+                new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC,
+                         0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            WPD_OBJECT_SIZE.pid = 11;
+            values.SetUnsignedLargeIntegerValue(WPD_OBJECT_SIZE, (ulong) fileInfo.Length);
+
+            var WPD_OBJECT_ORIGINAL_FILE_NAME = new _tagpropertykey();
+            WPD_OBJECT_ORIGINAL_FILE_NAME.fmtid =
+                new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC,
+                         0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            WPD_OBJECT_ORIGINAL_FILE_NAME.pid = 12;
+            values.SetStringValue(WPD_OBJECT_ORIGINAL_FILE_NAME, Path.GetFileName(fileName));
+
+            var WPD_OBJECT_NAME = new _tagpropertykey();
+            WPD_OBJECT_NAME.fmtid =
+                new Guid(0xEF6B490D, 0x5CD8, 0x437A, 0xAF, 0xFC,
+                         0xDA, 0x8B, 0x60, 0xEE, 0x4A, 0x3C);
+            WPD_OBJECT_NAME.pid = 4;
+            values.SetStringValue(WPD_OBJECT_NAME, Path.GetFileName(fileName));
+            
+            return values;
         }
         
         private static void EnumerateContents(ref IPortableDeviceContent content,
